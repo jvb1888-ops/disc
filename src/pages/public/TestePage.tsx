4e67d43eccfb00d82dbaf91f5553b0f8491be69d
jvb1888-ops <jvb1888@gmail.com>
2026-06-11 00:06:17 -0300

    Update TestePage.tsx

--- src/pages/public/TestePage.tsx
+++ src/pages/public/TestePage.tsx
@@ -75,8 +75,10 @@ export default function TestePage() {
 
       <header className={styles.header}>
         <div className={styles.headerLeft}>
-          <img src="/logo-dna.png" alt="DNA Comercial" className={styles.headerLogo} />
+          <div className={styles.logo}>D</div>
+          <span className={styles.logoText}>DISC Assessment</span>
         </div>
+        <img src="/logo-dna.png" alt="DNA Comercial" className={styles.headerLogoDna} />
         {nome && <span className={styles.userName}>Olá, {nome.split(' ')[0]} 👋</span>}
       </header>
 
